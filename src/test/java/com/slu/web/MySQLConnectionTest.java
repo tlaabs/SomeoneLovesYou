3fb5d2f72469f075aa3e10c32ec35f36cec4b132
tlaabs <tlaabs@naver.com>
2018-08-27 02:11:41 +0900

    Spring-Mybatis setting

---
+++ src/test/java/com/slu/web/MySQLConnectionTest.java
@@ -0,0 +1,31 @@
+package com.slu.web;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+
+public class MySQLConnectionTest {
+	private static final String DRIVER =
+			"com.mysql.jdbc.Driver";
+	private static final String URL =
+			"jdbc:mysql://127.0.0.1:3306/slu?useSSL=false&serverTimezone=UTC";
+	private static final String USER =
+			"sluser";
+	private static final String PW =
+			"0000";
+	
+	@Test
+	public void testConnection() throws Exception{
+		
+		Class.forName(DRIVER);
+		
+		try(Connection con = DriverManager.getConnection(URL, USER, PW)){
+			System.out.println(con);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+}
