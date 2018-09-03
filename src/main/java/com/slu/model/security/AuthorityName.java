eac82336547ac6b0aa2788006b1e9bfc8cb978bf
tlaabs <tlaabs@naver.com>
2018-09-04 01:12:27 +0900

    add model.secutiry package

---
+++ src/main/java/com/slu/model/security/AuthorityName.java
@@ -0,0 +1,5 @@
+package com.slu.model.security;
+
+public enum AuthorityName {
+    ROLE_USER, ROLE_ADMIN
+}
--- src/main/java/com/slu/security/service/JwtUserDetailsService.java
+++ src/main/java/com/slu/security/service/JwtUserDetailsService.java
@@ -3,9 +3,14 @@ package com.slu.security.service;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class JwtUserDetailsService implements UserDetailsService{
-
+	
+//	@Autowired
+//	private UserRepository userRepository;
+	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		// TODO Auto-generated method stub
