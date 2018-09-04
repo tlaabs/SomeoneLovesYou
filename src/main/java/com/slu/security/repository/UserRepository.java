7955b40fddcb4aff86a56605d5756490303fd5f8
tlaabs <tlaabs@naver.com>
2018-09-04 22:43:55 +0900

    troubleshooting problem when creating entityManagerFactory bean

--- src/main/java/com/slu/security/repository/UserRepository.java
+++ src/main/java/com/slu/security/repository/UserRepository.java
@@ -1,5 +1,10 @@
 package com.slu.security.repository;
 
-public interface UserRepository{
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.slu.model.security.User;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
--- src/main/java/com/slu/security/service/JwtUserDetailsService.java
+++ src/main/java/com/slu/security/service/JwtUserDetailsService.java
@@ -1,15 +1,18 @@
 package com.slu.security.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.slu.security.repository.UserRepository;
+
 @Service
 public class JwtUserDetailsService implements UserDetailsService{
 	
-//	@Autowired
-//	private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
