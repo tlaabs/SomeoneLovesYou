ecf656332bdabffb81984b9d40025bc04ab97ae5
tlaabs <tlaabs@naver.com>
2018-09-05 23:36:09 +0900

    implement getAuthenticatedUser but cannot join the h2 db

--- src/main/java/com/slu/controller/UserController.java
+++ src/main/java/com/slu/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.slu.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,19 +10,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.slu.security.JwtTokenUtil;
+import com.slu.security.JwtUser;
+
 @RestController
 public class UserController {
-	
+
 	@Value("${jwt.header}")
 	private String tokenHeader;
-	
-//	DB의 유저정보를 불러오는 역할을 한다.
+
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
+
+	//	DB의 유저정보를 불러오는 역할을 한다.
 	@Autowired
 	@Qualifier("jwtUserDetailsService")
 	private UserDetailsService userDetailsService;
-	
-	@RequestMapping(value="/user", method = RequestMethod.GET)
-	public String user(){
-		return tokenHeader;
+
+	@RequestMapping(value = "user", method = RequestMethod.GET)
+	public JwtUser getAuthenticatedUser(HttpServletRequest request) {
+		String token = request.getHeader(tokenHeader).substring(7);
+		String username = jwtTokenUtil.getUsernameFromToken(token);
+		JwtUser user = (JwtUser) userDetailsService.loadUserByUsername(username);
+		return user;
 	}
+
 }
